2a3e499269e0ec1ab1054779caa202a0e634f60d
Gabriel D <gabriel.drutu@gmail.com>
2021-01-03 09:16:47 +0200

    Create login - part 3

--- client/src/components/Register_login/login.js
+++ client/src/components/Register_login/login.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
 import FormField from '../utils/Form/formfield';
-import {update} from '../utils/Form/formActions';
+import {
+  update, 
+  generateData, 
+  isFormValid
+} from '../utils/Form/formActions';
 
 
 class Login extends Component {
@@ -53,8 +57,19 @@ class Login extends Component {
     })
   }
 
-  submitForm = () => {
+  submitForm = (event) => {
+    event.preventDefault();
 
+    let dataToSubmit = generateData(this.state.formdata, 'login');
+    let formIsValid = isFormValid(this.state.formdata, 'login');
+
+    if(formIsValid){
+      console.log(dataToSubmit);
+    } else {
+      this.setState({
+        formError: true
+      });
+    }
   }
 
   render() {
@@ -71,6 +86,17 @@ class Login extends Component {
             formdata={this.state.formdata.password}
             change={(el) => this.updateForm(el)}
           />
+
+          { 
+            this.state.formError ?
+              <div className="error_label">
+                Please check your data
+              </div>
+              : null
+          }
+
+          <button onClick={event => this.submitForm(event)}>Log in</button>
+
         </form>
       </div>
     );
